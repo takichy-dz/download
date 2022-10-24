f705cfd1ec5e68c093505250abecbd88e86a4f58
takichy-dz <75120902+takichy-dz@users.noreply.github.com>
2022-10-24 04:33:36 +0100

    Update emu_spider_x.sh

--- emu_spider_x.sh
+++ emu_spider_x.sh
@@ -11,9 +11,9 @@ tar xf EMU_OtexSpiderX_Takichy.tar.gz -C /
 set +e
 rm -f EMU_OtexSpiderX_Takichy.tar.gz
 cd ..
-chmod 755 /usr/bin/revcamv2
-chmod 755 /etc/init.d/softcam.RevCamV2
-chmod 755 /etc/RevCamV2.emu
+chmod 755 /usr/local/bin/oscam
+chmod 755 /data/gx/local/oscam-1/oscam
+chmod 755 /data/gx/local/oscam-3/oscam
 echo ""
 cd ..
 sync
